d9583c14fc07e23a85f73318bce305c8a98282eb
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-12 11:24:14 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ContentChild,
+  AfterContentInit
+} from '@angular/core';
+import { PersonComponent } from '../person/person.component';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css']
 })
-export class PlaygroundComponent implements OnInit {
+export class PlaygroundComponent implements OnInit, AfterContentInit {
+  @ContentChild(PersonComponent, { static: true }) person: PersonComponent;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log('constructor', this.person);
   }
 
+  ngOnInit() {
+    //  available only if {static: true}
+    console.log('ngOnInit', this.person);
+  }
+
+  ngAfterContentInit(): void {
+    //  correct way
+    console.log('ngAfterContentInit', this.person);
+  }
 }
