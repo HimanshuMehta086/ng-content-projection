2cb1567e623be313f8951803abe91b3875cdaa5e
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-12 11:34:54 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -12,6 +12,15 @@ import {
   styleUrls: ['./playground.component.css']
 })
 export class PlaygroundComponent implements OnInit, AfterContentInit {
+  //  ContentChild cannot randomly query the DOM elements in content projected
+  //  ContentChild can query components nevertheless
+
+  //  From documentation:
+  //  ContentChild does not retrieve elements or directives
+  //  that are in other components' templates,
+  //  since a component's template is always a
+  //  black box to its ancestors.
+
   @ContentChild('h2', { static: true }) header: ElementRef;
 
   constructor() {}
