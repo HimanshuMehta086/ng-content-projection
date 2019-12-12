295fc453a0d582850d3146a1d83487bc401e0eff
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-12 10:44:47 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PlaygroundComponent } from './playground/playground.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PlaygroundComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule
